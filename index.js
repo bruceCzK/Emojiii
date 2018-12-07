ceda796cdd3387f6bdeec395194db8087a403fcb
陈卓恺 <bruceczk@foxmail.com>
2018-12-07 14:07:04 +0800

    Remove gender emoji from regex and images

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
  * Created by chenzhuokai on 16/6/2.
  */
 (function () {
-  'use strict';
-  const fs = require('fs-extra');
-  const path = require('path');
-  const Xray = require('x-ray');
-  const xray = Xray();
+  'use strict'
+  const fs = require('fs-extra')
+  const path = require('path')
+  const Xray = require('x-ray')
+  const xray = Xray()
 
   const emojiListUrl = [
     'https://ark.xinshu.me/pages/unicode/full-emoji-list.html',
-    'https://ark.xinshu.me/pages/unicode/full-emoji-modifiers.html',
+    'https://ark.xinshu.me/pages/unicode/full-emoji-modifiers.html'
     // Original URL
     // http://unicode.org/emoji/charts/full-emoji-list.html,
     // http://unicode.org/emoji/charts/full-emoji-modifiers.html
-  ];
+  ]
 
-  console.log('Fetching emoji list from', emojiListUrl);
-  console.log('--------');
+  console.log('Fetching emoji list from', emojiListUrl)
+  console.log('--------')
 
   const xrayConfig = {
     no: 'td:nth-child(1)',
@@ -33,31 +33,33 @@
       samsung: 'td:nth-child(10):not([colspan]) img@src',
       windows: 'td:nth-child(11):not([colspan]) img@src'
     }
-  };
+  }
 
   const selector = 'table[border] tr:nth-child(n+1)'
   Promise.all(emojiListUrl.map(url => fetch(url, selector, [xrayConfig]))).then(results => {
     results = results[0].concat(results[1])
-    console.log('Emoji list fetched, Emoji total count', results.length);
-    console.log('--------');
+    console.log('Emoji list fetched, Emoji total count', results.length)
+    console.log('--------')
 
     // exclude thead
-    results = results.filter(function (emoji) {
-      return !!emoji.unicode || !/^U/i.test(emoji.unicode);
-    });
+    results = results
+      .filter(emoji => {
+        return !!emoji.unicode || !/^U/i.test(emoji.unicode)
+      })
+      .filter(emoji => !!emoji.image.apple) // only apple has made
 
     // write image string to file
     try {
-      fs.emptyDirSync(path.join(__dirname, 'images'));
-      console.log('Writing images');
-      console.log('--------');
+      fs.emptyDirSync(path.join(__dirname, 'images'))
+      console.log('Writing images')
+      console.log('--------')
       fs.outputJsonSync(__dirname + '/emoji.json', results.map((i, index) => {
         return {
           no: index + 1,
           unicode: i.unicode,
           name: i.name
         }
-      }));
+      }))
 
       results.forEach(function (emoji) {
         let unicode = emoji.unicode
@@ -65,36 +67,36 @@
           .replace(/\s/g, '-')
           .replace(/^00/, '') // remove heading 00
           .replace(/fe0f-20e3/ig, '20e3') // fix keycap filename
-          .toLocaleLowerCase();
+          .toLocaleLowerCase()
 
         Object.keys(xrayConfig.image).forEach(function (type) {
           if (!emoji.image[type]) {
-            return;
+            return
           }
-          const buffer = decodeBase64Image(emoji.image[type]);
-          type = type.replace(/([A-Z])/g, '-$1').toLowerCase();
-          fs.outputFileSync(path.join(__dirname, 'images', type, unicode + '.png'), buffer);
-        });
-      });
+          const buffer = decodeBase64Image(emoji.image[type])
+          type = type.replace(/([A-Z])/g, '-$1').toLowerCase()
+          fs.outputFileSync(path.join(__dirname, 'images', type, unicode + '.png'), buffer)
+        })
+      })
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
 
-    console.log('Finished');
-    process.exit();
+    console.log('Finished')
+    process.exit()
   })
 
   function decodeBase64Image(dataString) {
-    const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    let response;
+    const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
+    let response
 
     if (matches.length !== 3) {
-      return new Error('Invalid input string');
+      return new Error('Invalid input string')
     }
 
-    response = new Buffer(matches[2], 'base64');
+    response = new Buffer(matches[2], 'base64')
 
-    return response;
+    return response
   }
 
   function fetch(url, selector, config) {
@@ -107,4 +109,4 @@
       })
     })
   }
-})();
+})()
--- regex.js
+++ regex.js
@@ -1,70 +1,67 @@
 /**
  * Created by chenzhuokai on 16/6/3.
  */
-(function () {
-  'use strict';
-  const fs = require('fs-extra');
-  const _ = require('lodash');
+const fs = require('fs-extra')
+const _ = require('lodash')
 
-  let emojiList = fs.readJsonSync(__dirname + '/emoji.json');
-  const fromCharCode = String.fromCharCode;
-  const unicodeToJsEscape = require('unicode-escape');
+let emojiList = fs.readJsonSync(__dirname + '/emoji.json')
+const fromCharCode = String.fromCharCode
+const unicodeToJsEscape = require('unicode-escape')
 
-  let unicodeList = emojiList.map(function (emoji) {
-    emoji.converted = emoji.unicode.replace(/U\+/g, '').replace(/\s/g, '-');
-    return emoji;
-  }).filter(emoji => {
-    return /U\+/.test(emoji.unicode)
-  });
+let unicodeList = emojiList.map(function (emoji) {
+  emoji.converted = emoji.unicode.replace(/U\+/g, '').replace(/\s/g, '-')
+  return emoji
+}).filter(emoji => /U\+/.test(emoji.unicode))
 
-  unicodeList = _.sortBy(unicodeList, function (unicode) {
-    return -(unicodeToJsEscape(unicode.converted).length);
-  });
+unicodeList = _.sortBy(unicodeList, function (unicode) {
+  return -(unicodeToJsEscape(unicode.converted).length)
+})
 
-  unicodeList = unicodeList.map(function (unicode) {
-    unicode.converted = unicodeToJsEscape(unicode.converted.split('-').map(fromCodePoint).join(''));
-    return unicode;
-  });
+unicodeList = unicodeList.map(function (unicode) {
+  unicode.converted = unicodeToJsEscape(unicode.converted.split('-').map(fromCodePoint).join(''))
+  return unicode
+})
 
-  console.log('Exporting regex-es5.txt');
-  let regexEs5Txt = unicodeList.map((u)=> {
-    return u.converted.replace('\\ufe0f\\u20e3', '\\ufe0f?\\u20e3');
-  }).join('|')
-  fs.outputFileSync(__dirname + '/regex-es5.txt', regexEs5Txt);
+console.log('Exporting json')
+fs.outputJsonSync(__dirname + '/unicode.json', unicodeList)
 
-  console.log('Exporting regex-es6.txt');
-  fs.outputFileSync(__dirname + '/regex-es6.txt', unicodeList.map((u)=> {
-    return u.unicode.replace(/\s/g, '').replace(/U\+([0-9A-F]+)/ig, '\\u{$1}').toLocaleLowerCase();
-  }).join('|'));
+console.log('Exporting regex-es5.txt')
+let regexEs5Txt = unicodeList.map((u) => {
+  return u.converted.replace('\\ufe0f\\u20e3', '\\ufe0f?\\u20e3')
+}).join('|')
+fs.outputFileSync(__dirname + '/regex-es5.txt', regexEs5Txt)
 
-  console.log('Exporting regex-python.txt');
-  fs.outputFileSync(__dirname + '/regex-python.txt', unicodeList.map((u)=> {
-    let unicode = u.unicode.replace(/\s/g, '');
-    return unicode.split('U+').map(char => {
-      if (!char) {
-        return ''
-      }
-      return '\\U' + _.padStart(char, 8, '0')
-    }).join('')
-  }).join('|'));
+console.log('Exporting regex-es6.txt')
+fs.outputFileSync(__dirname + '/regex-es6.txt', unicodeList.map((u) => {
+  return u.unicode.replace(/\s/g, '').replace(/U\+([0-9A-F]+)/ig, '\\u{$1}').toLocaleLowerCase()
+}).join('|'))
 
-  fs.outputJson(__dirname + '/unicode.json', unicodeList);
-
-  let jsFile = fs.readFileSync('./emoji-parser.js.template').toString()
-  jsFile = jsFile.replace('<% regex-es5 %>', regexEs5Txt)
-  jsFile = `/* Generated at ${new Date()} */\n` + jsFile
-  fs.writeFileSync('./emoji-parser.js', jsFile)
-
-  function fromCodePoint(codepoint) {
-    let code = typeof codepoint === 'string' ?
-      parseInt(codepoint, 16) : codepoint;
-    if (code < 0x10000) {
-      return fromCharCode(code);
+console.log('Exporting regex-python.txt')
+fs.outputFileSync(__dirname + '/regex-python.txt', unicodeList.map((u) => {
+  let unicode = u.unicode.replace(/\s/g, '')
+  return unicode.split('U+').map(char => {
+    if (!char) {
+      return ''
     }
-    code -= 0x10000;
-    return fromCharCode(
-      0xD800 + (code >> 10),
-      0xDC00 + (code & 0x3FF)
-    );
+    return '\\U' + _.padStart(char, 8, '0')
+  }).join('')
+}).join('|'))
+
+console.log('Exporting emoji-parser.js')
+let jsFile = fs.readFileSync('./emoji-parser.js.template').toString()
+jsFile = jsFile.replace('<% regex-es5 %>', regexEs5Txt)
+jsFile = `/* Generated at ${new Date()} */\n` + jsFile
+fs.outputFileSync('emoji-parser.js', jsFile)
+
+function fromCodePoint(codepoint) {
+  let code = typeof codepoint === 'string' ?
+    parseInt(codepoint, 16) : codepoint
+  if (code < 0x10000) {
+    return fromCharCode(code)
   }
-})();
+  code -= 0x10000
+  return fromCharCode(
+    0xD800 + (code >> 10),
+    0xDC00 + (code & 0x3FF)
+  )
+}
