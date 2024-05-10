3231a8e3e0094bdc6363930bd52064c6aa6c1d6c
Bruce <bruceczk@foxmail.com>
2024-05-10 16:39:04 +0800

    Change emoji source

--- index.js
+++ index.js
@@ -1,114 +1,77 @@
-/**
- * Created by chenzhuokai on 16/6/2.
- */
-(function () {
-  'use strict'
-  const fs = require('fs-extra')
-  const path = require('path')
-  const Xray = require('x-ray')
-  const xray = Xray()
+const fs = require('fs-extra')
+const path = require('path')
+const Xray = require('x-ray')
+const axios = require('axios')
+const {PromisePool} = require('@supercharge/promise-pool')
+const cliProgress = require('cli-progress')
 
-  const emojiListUrl = [
-    'https://ark.xinshu.me/pages/unicode/full-emoji-list.html',
-    'https://ark.xinshu.me/pages/unicode/full-emoji-modifiers.html'
-    // Original URL
-    // http://unicode.org/emoji/charts/full-emoji-list.html,
-    // http://unicode.org/emoji/charts/full-emoji-modifiers.html
-  ]
+const xray = Xray()
 
-  console.log('Fetching emoji list from', emojiListUrl)
-  console.log('--------')
+const emojiListUrl = {
+  apple: 'https://www.emojiall.com/zh-hans/image-emoji-platform/apple/hd'
+}
 
+main()
+
+async function main() {
+  const xrayItemSelector = '.emoji_image_box > .col'
   const xrayConfig = {
-    no: 'td:nth-child(1)',
-    name: 'td:nth-child(16)',
-    unicode: 'td:nth-child(2)',
-    image: {
-      apple: 'td:nth-child(4):not([colspan]) img@src',
-      google: 'td:nth-child(5):not([colspan]) img@src',
-      facebook: 'td:nth-child(6):not([colspan]) img@src',
-      windows: 'td:nth-child(7):not([colspan]) img@src',
-      twitter: 'td:nth-child(8):not([colspan]) img@src',
-      joyPixels: 'td:nth-child(9):not([colspan]) img@src',
-      samsung: 'td:nth-child(10):not([colspan]) img@src'
-    }
+    char: '.emoji_font',
+    link: 'a[href][download]@href',
+    name: 'a.strong.decoration[href][title]@title'
   }
 
-  const selector = 'table[border] tr:nth-child(n+1)'
-  Promise.all(emojiListUrl.map(url => fetch(url, selector, [xrayConfig]))).then(results => {
-    results = results[0].concat(results[1])
-    console.log('Emoji list fetched, Emoji total count', results.length)
+  for (const [brand, url] of Object.entries(emojiListUrl)) {
     console.log('--------')
-
-    // exclude thead
-    results = results
-      .filter(emoji => {
-        return !!emoji.unicode || !/^U/i.test(emoji.unicode)
-      })
-      .filter(emoji => !!emoji.image.apple) // only apple has made
-
-    // write image string to file
-    try {
-      fs.emptyDirSync(path.join(__dirname, 'images'))
-      console.log('Writing images')
-      console.log('--------')
-      fs.outputJsonSync(__dirname + '/emoji.json', results.map((i, index) => {
-        return {
-          no: index + 1,
-          unicode: i.unicode,
-          name: i.name
-        }
-      }))
-
-      results.forEach(function (emoji) {
-        let unicode = emoji.unicode
-          .replace(/u\+/ig, '')
-          .replace(/\s/g, '-')
-          .replace(/^00/, '') // remove heading 00
-          .toLocaleLowerCase()
-
-        Object.keys(xrayConfig.image).forEach(function (type) {
-          if (!emoji.image[type]) {
-            return
-          }
-          const buffer = decodeBase64Image(emoji.image[type])
-          type = type.replace(/([A-Z])/g, '-$1').toLowerCase()
-          fs.outputFileSync(path.join(__dirname, 'images', type, unicode + '.png'), buffer)
-          if (/fe0f-20e3/.test(unicode)) {
-            // fix keycap filename
-            fs.outputFileSync(path.join(__dirname, 'images', type, unicode.replace('fe0f-20e3', '20e3') + '.png'), buffer)
-          }
-        })
-      })
-    } catch (e) {
-      console.error(e)
+    console.log('Fetching %s emoji list from %s', brand, url)
+    const results = await xray(url, xrayItemSelector, [xrayConfig])
+    console.log('Emoji list fetched, Emoji total count', results.length)
+    for (const item of results) {
+      item.unicode = charToUnicode(item.char)
     }
-
-    console.log('Finished')
-    process.exit()
-  })
-
-  function decodeBase64Image(dataString) {
-    const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
-    let response
-
-    if (matches.length !== 3) {
-      return new Error('Invalid input string')
-    }
-
-    response = new Buffer(matches[2], 'base64')
-
-    return response
-  }
-
-  function fetch(url, selector, config) {
-    return new Promise((resolve, reject) => {
-      xray(url, selector, config)((err, results) => {
-        if (err) {
-          return reject(err)
-        }
-        resolve(results)
-      })
+    const json = results.map((i, idx) => {
+      return {
+        no: idx + 1,
+        name: i.name,
+        unicode: i.unicode
+      }
     })
+    fs.outputJsonSync(path.join(__dirname, 'emoji.json'), json)
+    console.log('Json file saved')
+
+    let leftCount = results.length
+    let fileCount = 0
+
+    console.log('Fetching images files %d', leftCount)
+
+    const b1 = new cliProgress.SingleBar()
+    b1.start(leftCount)
+
+    await PromisePool.for(results).withConcurrency(64).process(async item => {
+      const {data: arraybuffer} = await axios.get(item.link, {responseType: 'arraybuffer'})
+      const filename = path.basename(item.link)
+      const pathname = path.join(__dirname, 'images', brand, filename)
+      if (/fe0f-20e3/.test(pathname)) {
+        // fix keycap filename
+        fileCount++
+        await fs.outputFile(pathname.replace('fe0f-20e3', '20e3'), arraybuffer)
+      }
+      if (/-fe0f\.png$/.test(filename) && filename.split('-').length === 2) {
+        // fix fe0f suffix
+        fileCount++
+        await fs.outputFile(pathname.replace(/-fe0f\.png$/, '.png'), arraybuffer)
+      }
+      fileCount++
+      b1.increment()
+      return fs.outputFile(pathname, arraybuffer)
+    })
+
+    b1.stop()
+
+    console.log('%d files saved', fileCount)
   }
-})()
+}
+
+function charToUnicode(char) {
+  return [...char].map(e => 'U+' + e.codePointAt(0).toString(16).toUpperCase()).join(' ')
+}
